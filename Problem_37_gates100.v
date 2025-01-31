63ab69468532e9ae42a78c7b2024b6c1f99e99d0
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2025-01-31 03:38:02 -0600

    Update Problem_37_gates100.v

--- Problem_37_gates100.v
+++ Problem_37_gates100.v
@@ -4,7 +4,7 @@ module top_module(
     output out_or,  // output of a 100-input OR gate
     output out_xor  // output of a 100-input XOR gate
 );
-	  // Reduction operators are great in instances where you need high input gate functionality
+    // Reduction operators are great in instances where you need high input gate functionality
     // With a reduction operator you can operate on every bit in a vector with the same operation specified in the reduction operator
     assign out_and = &in;
     assign out_or  = |in;
