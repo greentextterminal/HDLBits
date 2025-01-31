b7ce11aedb63050ea62c0eded995a02d7a5e348a
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2025-01-31 02:25:01 -0600

    Update Problem_34_always_nolatches.v

--- Problem_34_always_nolatches.v
+++ Problem_34_always_nolatches.v
@@ -6,19 +6,19 @@ module top_module (
     output reg up  ); 
     
     // 16'he06b	left arrow
-	  // 16'he072	down arrow
-	  // 16'he074	right arrow
-	  // 16'he075	up arrow
+    // 16'he072	down arrow
+    // 16'he074	right arrow
+    // 16'he075	up arrow
     // Anything else none (0)
     
     always @ (*) begin
       // equivalent to a prior default, makes default case redundant, and helps prevent creating latches
-   		up = 1'b0; down = 1'b0; left = 1'b0; right = 1'b0; 
+      up = 1'b0; down = 1'b0; left = 1'b0; right = 1'b0; 
       case (scancode)
         16'he06b : left  = 1;
-			  16'he072 : down  = 1;	
-			  16'he074 : right = 1;
-			  16'he075 : up    = 1;	
+	16'he072 : down  = 1;	
+	16'he074 : right = 1;
+	16'he075 : up    = 1;	
       endcase
     end
 
