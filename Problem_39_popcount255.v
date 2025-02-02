2560f57049e52e81dea0472f4b143d9431d26501
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2025-02-01 19:25:59 -0600

    Update Problem_39_popcount255.v

--- Problem_39_popcount255.v
+++ Problem_39_popcount255.v
@@ -5,7 +5,7 @@ module top_module(
     // Building a "population count" circuit to count the number of 1's in an input vector
     // In this solution we are using a for loop to check all 255 bit indices for a 1 bit value
     // If the bit index contains a 1 we increment our out variable
- 	  // It is important to remember that we must initialize our out to 0 to prevent inferring a latch if for instance an all 0 input is provided
+    // It is important to remember that we must initialize our out to 0 to prevent inferring a latch if for instance an all 0 input is provided
     
     reg [7:0] i; // declare incrementer
     always @ (*) begin
