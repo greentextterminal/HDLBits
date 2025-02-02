2bca4832b858440f86361ac9fb9a32f1d9eb1917
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2025-02-01 19:03:51 -0600

    Update Problem_40_adder100i.v

--- Problem_40_adder100i.v
+++ Problem_40_adder100i.v
@@ -9,12 +9,12 @@ module top_module(
     // since our full adder deals with 2 bits at a time (200 bits / (2 bits / 1 FA))) we need 100 full adder instantiations
     // 2BFA : 2 Bit Full Adder
     // Below is a visualization of the 100 bit ripple carry adder chain we are implementing
-    //         ___________         			    ___________					                        ___________	
-    //         |2BFA_0   |         			    |2BFA_1   |					         .....          |2BFA_99  |	
+    //         ___________                  ___________					                ___________	
+    //         |2BFA_0   |                  |2BFA_1   |                  .....          |2BFA_99  |	
     // cin  -> |cin  cout|--cout[0]------->	|cin  cout|--cout[1]-------> ..... -------> |cin  cout|--cout[99]-------> 
     // a[0] -> |a    sum |->sum[0]	a[1] -> |a    sum |->sum[1]          ..... a[98] -> |a    sum |->sum[99]
-    // b[0] -> |b        |		  	  b[1] ->	|b        |					         ..... b[98] -> |b        |		
-    //         |_________|					        |_________|					         .....          |_________|	
+    // b[0] -> |b        |          b[1] ->	|b        |	                 ..... b[98] -> |b        |		
+    //         |_________|                  |_________|                  .....          |_________|	
     //
     // In order to create the above configuration in a neatly packaged for loop we need to think about how we handle our cin and couts.
     // Our for loop can create as many copies of our 2 bit full adder as we'd like but how can we handle the initial cin into
