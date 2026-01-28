092872fd97a871c96350fb96e9d018c14f84e2b1
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2026-01-28 01:56:30 -0600

    Create Problem_82_8DFlipFlops.v

---
+++ Problem_82_8DFlipFlops.v
@@ -0,0 +1,20 @@
+/*
+Create 8 D flip-flops. All DFFs should be triggered by the positive edge of clk.
+*/
+
+module top_module (
+    input clk,
+    input [7:0] d,
+    output [7:0] q
+);
+
+  genvar i;
+  generate 
+    for (i=0; i<8; i=i+1) begin : DFF8
+      always @ (posedge clk) begin
+        q[i] <= d[i];
+      end
+    end
+  endgenerate
+
+endmodule
