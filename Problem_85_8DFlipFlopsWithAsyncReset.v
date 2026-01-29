bf35c76a521454d652f7181ed05e4b3e9af5fa3b
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2026-01-28 20:18:24 -0600

    Create Problem_85_8DFlipFlopsWithAsyncReset.v

---
+++ Problem_85_8DFlipFlopsWithAsyncReset.v
@@ -0,0 +1,22 @@
+/*
+Create 8 D flip-flops with active high asynchronous reset. All DFFs should be triggered by the positive edge of clk.
+*/
+
+module top_module (
+    input clk,
+    input areset,   // active high asynchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+
+  // by keeping the areset in the sensitivity list you achieve asynchronous behavior
+  always @ (posedge clk or posedge areset) begin
+    if (areset) begin
+      q <= 8'h00;
+    end
+    else begin
+      q <= d;
+    end
+  end
+
+endmodule
