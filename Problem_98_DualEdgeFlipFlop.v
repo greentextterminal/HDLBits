e55a8737a27e9797e34e90492ef2d33b682aef9f
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2026-02-01 19:20:52 -0600

    Update Problem_98_DualEdgeFlipFlop.v

--- Problem_98_DualEdgeFlipFlop.v
+++ Problem_98_DualEdgeFlipFlop.v
@@ -15,22 +15,32 @@ module top_module (
   The posedge clk automatically makes the flop flop positive edge clock triggered because of the (posedge clk) sensitivity list
   Need to implement a negative edge detector to get the flop to "react" to the negative edge of the clk
   */
-  // negative edge detector
-  reg clk_dly;
-  wire clk_negedge;
-  assign clk_negedge = ~clk & clk_dly;
-
-  reg posedge_clk_dff;
-  reg negedge_clk_dff;
-
-  assign q = clk_negedge ? negedge_clk_dff : posedge_clk_dff;
-  
-  always @ (posedge clk) begin
-    clk_dly <= clk;
-    posedge_clk_dff <= d;
-    if (clk_negedge) begin
-      negedge_clk_dff <= d;
+    
+    // negedge detector
+    reg clk_dly;
+    wire clk_negedge;
+    assign clk_negedge = ~clk & clk_dly; // use the clk_negedge as the select line
+    
+    // for the negedge detector
+    always @ (posedge clk) begin
+    	clk_dly <= clk; // delayed clk signal
     end
-  end
+
+    // create two flops to sample at different clock edges
+    reg posedge_dff;
+    reg negedge_dff;
+
+    // sample d at posedge of the clk
+    always @ (posedge clk) begin
+    	posedge_dff <= d;
+    end
+
+    // sample d at negative of the clk
+    always @ (posedge clk_negedge) begin
+        negedge_dff <= d;
+    end
+
+    // multiplex the output by selecting the flop correposonding to the clk edge (clk is the select line)
+    assign q = clk ? posedge_dff : negedge_dff;
 
 endmodule
