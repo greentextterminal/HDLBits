ce4163ff31cebd47881b61a6fad9a91e2ef4c935
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2026-06-04 00:17:33 -0500

    Create Problem_137_FSMSerialWithDatapathAndParityChecking.v

---
+++ Problem_137_FSMSerialWithDatapathAndParityChecking.v
@@ -0,0 +1,143 @@
+/*
+We want to add parity checking to the serial receiver. Parity checking adds one extra bit after each data byte. 
+We will use odd parity, where the number of 1s in the 9 bits received must be odd. 
+For example, 101001011 satisfies odd parity (there are 5 1s), but 001001011 does not.
+
+Change your FSM and datapath to perform odd parity checking. 
+Assert the done signal only if a byte is correctly received and its parity check passes. 
+Like the serial receiver FSM, this FSM needs to identify the start bit, wait for all 9 (data and parity) bits, then verify that the stop bit was correct. 
+If the stop bit does not appear when expected, the FSM must wait until it finds a stop bit before attempting to receive the next byte.
+
+You are provided with the following module that can be used to calculate the parity of the input stream (It's a TFF with reset). 
+The intended use is that it should be given the input bit stream, and reset at appropriate times so it counts the number of 1 bits in each byte.
+*/
+
+module top_module(
+    input clk,
+    input in,
+    input reset,    // Synchronous reset
+    output [7:0] out_byte,
+    output done
+); 
+  // params for counter
+  localparam DATA_LENGTH = 8;
+
+  // state encoding
+  localparam [2:0] IDLE  = 0,
+                   START = 1,
+                   DATA  = 2,
+                   STOP  = 3,
+                   WAIT  = 4;
+
+  // reg and wire for counter
+  reg [$clog2(DATA_LENGTH+1)-1:0] count;
+  wire count_hit;
+
+  // registers for state
+  reg [2:0] state, next_state;
+
+  // counter cycles limit hit
+  assign count_hit = (count == (DATA_LENGTH - 1));
+
+  // counter
+  /*
+    @ CC 1 -> count = 0 
+    @ CC 2 -> count = 1
+    ...
+    @ CC 7 -> count = 7
+  */
+  always @ (posedge clk) begin
+    if (reset) begin
+      count <= 0;
+    end
+    else if (count_hit) begin // counter 0 reset takes higher priority
+      count <= 0;
+    end
+    else if (state == DATA) begin // enable the count if in DATA state
+      count <= count + 1;
+    end
+    else begin
+      count <= count;
+    end
+  end
+
+  // state always block
+  always @ (posedge clk) begin
+    if (reset) begin
+      state <= IDLE;
+    end
+    else begin
+      state <= next_state;
+    end
+  end
+
+  /*
+      [IDLE] -(~in)-> [START] -(1 CC)-> [DATA] -(8CC) & (in)-> [STOP]
+                                           |                    |  |__(~in)__>[START]
+                                         (~in)                  |______(in)__>[IDLE]
+                                           |
+                                         [WAIT]--(in)-->[IDLE]
+
+  */
+
+  // next state transition logic
+  always @ (*) begin
+     // hold the current state to prevent latches
+    next_state = state;
+    
+    case (state)
+      IDLE: begin
+        if (~in)
+          next_state = START;
+      end
+      START: begin
+        // 1 CC later
+        next_state = DATA;
+      end
+      DATA: begin
+        // wait for 8 CCs of data
+        if (count_hit) begin
+          if (in)
+            next_state = STOP;
+          else
+            next_state = WAIT;
+        end
+      end
+      STOP: begin
+        if (in)
+          next_state = IDLE;
+        else
+          next_state = START;
+      end
+      WAIT: begin
+        if (in)
+          next_state = IDLE;
+      end
+      default: begin // illegal state handling
+        next_state = IDLE;
+      end
+    endcase
+  end
+
+  // driving output (needs to be a pulse)
+  assign done = (state == STOP);
+
+  // 1 byte register for data shifting
+  reg [7:0] output_byte;
+
+  // always clocked block to shift in data
+  always @ (posedge clk) begin
+    if (reset)
+      output_byte <= 0;
+    // shift in data during the exact clock cycle we enter DATA state
+    // check for !count_hit to stop shifting in data at the last bit of the payload (safety measure)
+    else if ((next_state == DATA) && (!count_hit))
+      output_byte <= {in, output_byte[7:1]};
+    else
+      output_byte <= output_byte; // hold the data
+  end
+
+  // send out the shifted in byte
+  assign out_byte = (state == STOP) ? output_byte : (8'b0);
+  
+endmodule
