ec47ab7ef8296480c4204a2f356b07d93ad35525
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2026-08-24 21:28:19 -0500

    Update Problem_180_TwoBitBranchPredictor.v

--- Problem_180_TwoBitBranchPredictor.v
+++ Problem_180_TwoBitBranchPredictor.v
@@ -15,73 +15,78 @@ module top_module(
                    WT  = 2'd2,
                    ST  = 2'd3;
 
-  // Bundling the train_valid and train_takan to create managable localparam states
-  
-
   // FSM regs
   /* FSM Logic
-      [combo logic] -> [state] -> [next_state]
-            ^__________________________|
+      [combo logic] -> [next_state] -> [state]
+            ^_____________________________|
   */
-  reg [1:0] curr_state, next_state;
+  reg [1:0] current_state, next_state;
 
   // next state logic
-  always @ (posedge clk) begin
-      next_state <= curr_state;
+  always @ (posedge clk or posedge areset) begin
+      if (areset) begin
+          current_state <= WNT; // areset returns to WNT state
+      end
+      else begin
+          current_state <= next_state; // current state is fed by next state, which is determined by the combo logic case statements
+      end
   end
 
-  // combo logic that determines next_state based on current state
+  // --------combo logic that determines next_state based on current state--------
   // train_valid & train_taken  -> incrment
   // train_valid & ~train_taken -> decrement
   // ~train_valid               -> hold state
   always @ (*) begin
-    // adding a default assign statement to prevent latches (assign state to itself to hold last state to handle SNT and ST cases)
-      assign curr_state <= curr_state; // handles the train_valid = 0 case
+      // adding a default assign statement to prevent latches (assign state to itself to hold last state to handle SNT and ST cases)
+      next_state = current_state; // handles the train_valid = 0 case (we are deliberately creating a latch)
     
-      case (next_state)
+      case (current_state)
           SNT: begin
               if (train_valid) begin
                   if (train_taken) begin
-                      curr_state <= WNT;
+                      next_state = WNT; // increment to next state
+                  end
+                  else begin
+                      next_state = current_state; // hold state since no other state to decrement to
                   end
               end
           end
           WNT: begin
               if (train_valid) begin
                   if (train_taken) begin
-                      curr_state <= WT;
+                      next_state = WT; // increment to next state
                   end
                   else begin
-                      curr_state <= SNT;
+                      next_state = SNT; // decrement state
                   end
               end
           end
           WT: begin
               if (train_valid) begin
                   if (train_taken) begin
-                      curr_state <= ST;
+                      next_state = ST; // increment to next state
                   end
                   else begin
-                      curr_state <= WNT;
+                      next_state = WNT; // decrement state
                   end
               end
           end
           ST: begin
               if (train_valid) begin
                   if (train_taken) begin
-                      curr_state <= state;
+                      next_state = current_state; // hold state since no other state to increment to
                   end
                   else begin
-                      curr_state <= WT;
+                      next_state = WT; // decrement state
                   end
               end
           end
           default: begin
-              next_state <= WNT; // this is the reset value, therefore its the default
+              next_state = WNT; // this is the reset value, therefore its the default
           end
       endcase 
     end
-
+    
     // drive output with current state
-    assign state = curr_state;
+    assign state = current_state;
 endmodule
